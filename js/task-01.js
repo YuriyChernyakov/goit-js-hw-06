7522a2f4318e6b559b347538603a4047d41f2249
Yuriy Chernyakov <nitro_06@ukr.net>
2022-12-10 22:00:03 +0200

    test 1

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const allCategories = document.getElementsByTagName('ul');
+console.log('Number of categories:', allCategories.length);
+
+const category1 = document.querySelector('h2');
+console.log('Category:',category1.textContent);
+
+const element = category1.length;
+console.log(element)
