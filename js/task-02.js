8e4a392d99c7f21d13db38acdf8780bdf8e398d5
Yuriy Chernyakov <nitro_06@ukr.net>
2023-01-19 17:24:13 +0200

    final v1.5

--- js/task-02.js
+++ js/task-02.js
@@ -8,10 +8,13 @@ const ingredients = [
 ];
 
 const list = document.querySelector('#ingredients');
+const newArray = [];
 
-for (let i = 0; i < ingredients.length; i += 1) {
-  const li = document.createElement('li');
-  li.innerHTML = ingredients[i];
-  li.classList.add('item');
-  list.append(li);
+for (let i = 0; i < ingredients.length; i += 1) { 
+ const li = document.createElement('li');
+ li.innerHTML = ingredients[i];
+ li.classList.add('item');
+newArray.push(li)
 }
+ 
+list.append(...newArray);
